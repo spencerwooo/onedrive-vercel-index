4c7b27a886c56fdff83a79fb54d915f76e85dae7
spencerwooo <spencer.woo@outlook.com>
2021-08-23 16:26:04 +0100

    revert to original useSWR

--- components/FileListing.tsx
+++ components/FileListing.tsx
@@ -109,9 +109,7 @@ const FileListing: FunctionComponent<{ query?: ParsedUrlQuery }> = ({ query }) =
 
   const path = queryToPath(query)
 
-  const { data, error } = useSWR(`/api?path=${path}`, fetcher, {
-    revalidateOnFocus: false,
-  })
+  const { data, error } = useSWR(`/api?path=${path}`, fetcher)
 
   if (error) {
     return (
--- components/previews/MarkdownPreview.tsx
+++ components/previews/MarkdownPreview.tsx
@@ -17,14 +17,7 @@ import DownloadBtn from '../DownloadBtn'
 const fetcher = (url: string) => axios.get(url).then(res => res.data)
 
 const MarkdownPreview: FunctionComponent<{ file: any; standalone?: boolean }> = ({ file, standalone = true }) => {
-  const { data, error } = useSWR(file['@microsoft.graph.downloadUrl'], fetcher, {
-    revalidateOnFocus: false,
-    revalidateOnMount: false,
-    revalidateOnReconnect: false,
-    refreshWhenOffline: false,
-    refreshWhenHidden: false,
-    refreshInterval: 0,
-  })
+  const { data, error } = useSWR(file['@microsoft.graph.downloadUrl'], fetcher)
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
