aa848c92f81ac97b4d1de7317e7bef9a96262081
myl7 <myl@myl.moe>
2022-02-06 17:26:46 +0800

    add trailing slash config to fix i18n with api routes

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ const { i18n } = require('./next-i18next.config')
 
 module.exports = {
   i18n,
-  reactStrictMode: true
+  reactStrictMode: true,
+  // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
+  trailingSlash: true
 }
