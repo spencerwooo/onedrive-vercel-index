67298cd1173e42cbdcdc86f34e680c7c795a6220
Zkeq <learnonly@learnonly.xyz>
2022-02-07 19:40:09 +0800

    Avoid opening browser search box in Chrome when pressing Ctrl+K

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,7 +23,10 @@ const Navbar = () => {
   const [searchOpen, setSearchOpen] = useState(false)
   const openSearchBox = () => setSearchOpen(true)
 
-  useHotkeys(`${os === 'mac' ? 'cmd' : 'ctrl'}+k`, openSearchBox)
+  useHotkeys(`${os === 'mac' ? 'cmd' : 'ctrl'}+k`, e => {
+    openSearchBox()
+    e.preventDefault()
+  })
 
   useEffect(() => {
     const storedToken = () => {
