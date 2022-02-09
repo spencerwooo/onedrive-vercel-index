68bfe35a9c327c9be6da00a666866c6e7ae5fb35
Zkeq <learnonly@learnonly.xyz>
2022-02-09 10:10:30 +0800

    去除搜索功能

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,23 +10,26 @@ import { useRouter } from 'next/router'
 import { Fragment, useEffect, useState } from 'react'
 
 import siteConfig from '../config/site.config'
-import SearchModal from './SearchModal'
+// 去除搜索
+// import SearchModal from './SearchModal'
 import useDeviceOS from '../utils/useDeviceOS'
 
 const Navbar = () => {
   const router = useRouter()
-  const os = useDeviceOS()
+  // 去除搜索
+  // const os = useDeviceOS()
 
   const [tokenPresent, setTokenPresent] = useState(false)
   const [isOpen, setIsOpen] = useState(false)
 
-  const [searchOpen, setSearchOpen] = useState(false)
-  const openSearchBox = () => setSearchOpen(true)
+  // 去除搜索
+  // const [searchOpen, setSearchOpen] = useState(false)
+  // const openSearchBox = () => setSearchOpen(true)
 
-  useHotkeys(`${os === 'mac' ? 'cmd' : 'ctrl'}+k`, e => {
-    openSearchBox()
-    e.preventDefault()
-  })
+  // useHotkeys(`${os === 'mac' ? 'cmd' : 'ctrl'}+k`, e => {
+  //   openSearchBox()
+  //   e.preventDefault()
+  // })
 
   useEffect(() => {
     const storedToken = () => {
@@ -54,37 +57,153 @@ const Navbar = () => {
   }
 
   return (
-    <div className="sticky top-0 z-[100] border-b border-gray-900/10 bg-white bg-opacity-80 backdrop-blur-md dark:border-gray-500/30 dark:bg-gray-900">
+  //   <div className="sticky top-0 z-[100] border-b border-gray-900/10 bg-white bg-opacity-80 backdrop-blur-md dark:border-gray-500/30 dark:bg-gray-900">
+  //     <Toaster />
+
+  //     <SearchModal searchOpen={searchOpen} setSearchOpen={setSearchOpen} />
+
+  //     <div className="mx-auto flex w-full items-center justify-between space-x-4 px-4 py-1">
+  //       <Link href="/" passHref>
+  //         <a className="flex items-center space-x-2 py-2 hover:opacity-80 dark:text-white md:p-2">
+  //           <Image src={siteConfig.icon} alt="icon" width="25" height="25" priority />
+  //           <span className="hidden font-bold sm:block">{siteConfig.title}</span>
+  //         </a>
+  //       </Link>
+
+  //       <div className="flex flex-1 items-center space-x-4 text-gray-700 md:flex-initial">
+  //         <button
+  //           className="flex flex-1 items-center justify-between rounded-lg bg-gray-100 px-2.5 py-1.5 hover:opacity-80 dark:bg-gray-800 dark:text-white md:w-48"
+  //           onClick={openSearchBox}
+  //         >
+  //           <div className="flex items-center space-x-2">
+  //             <FontAwesomeIcon className="h-4 w-4" icon="search" />
+  //             <span className="text-sm font-medium">Search ...</span>
+  //           </div>
+
+  //           <div className="flex items-center space-x-1">
+  //             <div className="rounded-lg bg-gray-200 px-2 py-1 text-xs font-medium dark:bg-gray-700">
+  //               {os === 'mac' ? '⌘' : 'Ctrl'}
+  //             </div>
+  //             <div className="rounded-lg bg-gray-200 px-2 py-1 text-xs font-medium dark:bg-gray-700">K</div>
+  //           </div>
+  //         </button>
+
+  //         {siteConfig.links.length !== 0 &&
+  //           siteConfig.links.map((l: { name: string; link: string; other: string}) => (
+  //             <a
+  //               key={l.name}
+  //               href={l.link}
+  //               target="_blank"
+  //               rel="noopener noreferrer"
+  //               className="flex items-center space-x-2 hover:opacity-80 dark:text-white"
+  //             >
+  //               <FontAwesomeIcon icon={['fab', l.other.toLowerCase() as IconName]} />
+  //               <span className="hidden text-sm font-medium md:inline-block">{l.name}</span>
+  //             </a>
+  //           ))}
+
+  //         {siteConfig.email && (
+  //           <a href={siteConfig.email} className="flex items-center space-x-2 hover:opacity-80 dark:text-white">
+  //             <FontAwesomeIcon icon={['far', 'envelope']} />
+  //             <span className="hidden text-sm font-medium md:inline-block">Email</span>
+  //           </a>
+  //         )}
+
+  //         {tokenPresent && (
+  //           <button
+  //             className="flex items-center space-x-2 p-2 hover:opacity-80 dark:text-white"
+  //             onClick={() => setIsOpen(true)}
+  //           >
+  //             <span className="text-sm font-medium">Logout</span>
+  //             <FontAwesomeIcon icon="sign-out-alt" />
+  //           </button>
+  //         )}
+  //       </div>
+  //     </div>
+
+  //     <Transition appear show={isOpen} as={Fragment}>
+  //       <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" open={isOpen} onClose={() => setIsOpen(false)}>
+  //         <div className="min-h-screen px-4 text-center">
+  //           <Transition.Child
+  //             as={Fragment}
+  //             enter="ease-out duration-100"
+  //             enterFrom="opacity-0"
+  //             enterTo="opacity-100"
+  //             leave="ease-in duration-50"
+  //             leaveFrom="opacity-100"
+  //             leaveTo="opacity-0"
+  //           >
+  //             <Dialog.Overlay className="fixed inset-0 bg-gray-50 dark:bg-gray-800" />
+  //           </Transition.Child>
+
+  //           {/* This element is to trick the browser into centering the modal contents. */}
+  //           <span className="inline-block h-screen align-middle" aria-hidden="true">
+  //             &#8203;
+  //           </span>
+  //           <Transition.Child
+  //             as={Fragment}
+  //             enter="ease-out duration-100"
+  //             enterFrom="opacity-0 scale-95"
+  //             enterTo="opacity-100 scale-100"
+  //             leave="ease-in duration-50"
+  //             leaveFrom="opacity-100 scale-100"
+  //             leaveTo="opacity-0 scale-95"
+  //           >
+  //             <div className="my-8 inline-block w-full max-w-md transform overflow-hidden rounded-lg bg-white p-6 text-left align-middle transition-all dark:bg-gray-900">
+  //               <Dialog.Title className="text-lg font-bold text-gray-900 dark:text-gray-100">
+  //                 Clear all tokens?
+  //               </Dialog.Title>
+  //               <div className="mt-2">
+  //                 <p className="text-sm text-gray-500">
+  //                   These tokens are used to authenticate yourself into password protected folders, clearing them means
+  //                   that you will need to re-enter the passwords again.
+  //                 </p>
+  //               </div>
+
+  //               <div className="mt-4 max-h-32 overflow-y-scroll font-mono text-sm dark:text-gray-100">
+  //                 {siteConfig.protectedRoutes.map((r, i) => (
+  //                   <div key={i} className="flex items-center space-x-1">
+  //                     <FontAwesomeIcon icon="key" />
+  //                     <span className="truncate">{r}</span>
+  //                   </div>
+  //                 ))}
+  //               </div>
+
+  //               <div className="mt-8 flex items-center justify-end">
+  //                 <button
+  //                   className="mr-3 inline-flex items-center justify-center space-x-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300"
+  //                   onClick={() => setIsOpen(false)}
+  //                 >
+  //                   Cancel
+  //                 </button>
+  //                 <button
+  //                   className="inline-flex items-center justify-center space-x-2 rounded bg-red-500 px-4 py-2 text-white hover:bg-red-400 focus:outline-none focus:ring focus:ring-red-300"
+  //                   onClick={() => clearTokens()}
+  //                 >
+  //                   <FontAwesomeIcon icon={['far', 'trash-alt']} />
+  //                   <span>Clear all</span>
+  //                 </button>
+  //               </div>
+  //             </div>
+  //           </Transition.Child>
+  //         </div>
+  //       </Dialog>
+  //     </Transition>
+  //   </div>
+  // )
+  // 去除搜索
+      <div className="bg-white dark:bg-gray-900 dark:border-gray-500/30 sticky top-0 bg-opacity-80 border-b border-gray-900/10 backdrop-blur-md z-[100]">
       <Toaster />
 
-      <SearchModal searchOpen={searchOpen} setSearchOpen={setSearchOpen} />
-
-      <div className="mx-auto flex w-full items-center justify-between space-x-4 px-4 py-1">
+      <div className="flex items-center justify-between w-full mx-auto px-4 py-1">
         <Link href="/" passHref>
-          <a className="flex items-center space-x-2 py-2 hover:opacity-80 dark:text-white md:p-2">
+          <a className="dark:text-white hover:opacity-80 flex items-center p-2 space-x-2">
             <Image src={siteConfig.icon} alt="icon" width="25" height="25" priority />
-            <span className="hidden font-bold sm:block">{siteConfig.title}</span>
+            <span className="sm:block hidden font-bold">{siteConfig.title}</span>
           </a>
         </Link>
 
-        <div className="flex flex-1 items-center space-x-4 text-gray-700 md:flex-initial">
-          <button
-            className="flex flex-1 items-center justify-between rounded-lg bg-gray-100 px-2.5 py-1.5 hover:opacity-80 dark:bg-gray-800 dark:text-white md:w-48"
-            onClick={openSearchBox}
-          >
-            <div className="flex items-center space-x-2">
-              <FontAwesomeIcon className="h-4 w-4" icon="search" />
-              <span className="text-sm font-medium">Search ...</span>
-            </div>
-
-            <div className="flex items-center space-x-1">
-              <div className="rounded-lg bg-gray-200 px-2 py-1 text-xs font-medium dark:bg-gray-700">
-                {os === 'mac' ? '⌘' : 'Ctrl'}
-              </div>
-              <div className="rounded-lg bg-gray-200 px-2 py-1 text-xs font-medium dark:bg-gray-700">K</div>
-            </div>
-          </button>
-
+        <div className="flex items-center space-x-4 text-gray-700">
           {siteConfig.links.length !== 0 &&
             siteConfig.links.map((l: { name: string; link: string; other: string}) => (
               <a
@@ -92,23 +211,23 @@ const Navbar = () => {
                 href={l.link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex items-center space-x-2 hover:opacity-80 dark:text-white"
+                className="flex items-center space-x-2 dark:text-white hover:opacity-80"
               >
                 <FontAwesomeIcon icon={['fab', l.other.toLowerCase() as IconName]} />
-                <span className="hidden text-sm font-medium md:inline-block">{l.name}</span>
+                <span className="text-sm font-medium hidden md:inline-block">{l.name}</span>
               </a>
             ))}
 
           {siteConfig.email && (
-            <a href={siteConfig.email} className="flex items-center space-x-2 hover:opacity-80 dark:text-white">
+            <a href={siteConfig.email} className="flex items-center space-x-2 dark:text-white hover:opacity-80">
               <FontAwesomeIcon icon={['far', 'envelope']} />
-              <span className="hidden text-sm font-medium md:inline-block">Email</span>
+              <span className="text-sm font-medium hidden md:inline-block">Email</span>
             </a>
           )}
 
           {tokenPresent && (
             <button
-              className="flex items-center space-x-2 p-2 hover:opacity-80 dark:text-white"
+              className="dark:text-white flex items-center p-2 space-x-2 hover:opacity-80"
               onClick={() => setIsOpen(true)}
             >
               <span className="text-sm font-medium">Logout</span>
@@ -130,7 +249,7 @@ const Navbar = () => {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Dialog.Overlay className="fixed inset-0 bg-gray-50 dark:bg-gray-800" />
+              <Dialog.Overlay className="bg-gray-50 dark:bg-gray-800 fixed inset-0" />
             </Transition.Child>
 
             {/* This element is to trick the browser into centering the modal contents. */}
@@ -146,8 +265,8 @@ const Navbar = () => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="my-8 inline-block w-full max-w-md transform overflow-hidden rounded-lg bg-white p-6 text-left align-middle transition-all dark:bg-gray-900">
-                <Dialog.Title className="text-lg font-bold text-gray-900 dark:text-gray-100">
+              <div className="dark:bg-gray-900 inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white rounded-lg">
+                <Dialog.Title className="dark:text-gray-100 text-lg font-bold text-gray-900">
                   Clear all tokens?
                 </Dialog.Title>
                 <div className="mt-2">
@@ -157,7 +276,7 @@ const Navbar = () => {
                   </p>
                 </div>
 
-                <div className="mt-4 max-h-32 overflow-y-scroll font-mono text-sm dark:text-gray-100">
+                <div className="dark:text-gray-100 max-h-32 mt-4 overflow-y-scroll font-mono text-sm">
                   {siteConfig.protectedRoutes.map((r, i) => (
                     <div key={i} className="flex items-center space-x-1">
                       <FontAwesomeIcon icon="key" />
@@ -166,15 +285,15 @@ const Navbar = () => {
                   ))}
                 </div>
 
-                <div className="mt-8 flex items-center justify-end">
+                <div className="flex items-center justify-end mt-8">
                   <button
-                    className="mr-3 inline-flex items-center justify-center space-x-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300"
+                    className="focus:outline-none focus:ring focus:ring-blue-300 hover:bg-blue-400 inline-flex items-center justify-center px-4 py-2 mr-3 space-x-2 text-white bg-blue-500 rounded"
                     onClick={() => setIsOpen(false)}
                   >
                     Cancel
                   </button>
                   <button
-                    className="inline-flex items-center justify-center space-x-2 rounded bg-red-500 px-4 py-2 text-white hover:bg-red-400 focus:outline-none focus:ring focus:ring-red-300"
+                    className="focus:outline-none focus:ring focus:ring-red-300 hover:bg-red-400 inline-flex items-center justify-center px-4 py-2 space-x-2 text-white bg-red-500 rounded"
                     onClick={() => clearTokens()}
                   >
                     <FontAwesomeIcon icon={['far', 'trash-alt']} />
