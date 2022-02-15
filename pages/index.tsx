261603639f2ee8c32de084ae4118cf6b0aca2cdb
Zkeq <learnonly@learnonly.xyz>
2022-02-15 21:05:38 +0800

    不知道加哪

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,6 @@ import Script from 'next/script'
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-gray-900">
-      <Script src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></Script>
       <Head>
         <title>{siteConfig.title}</title>
       </Head>
@@ -23,14 +22,12 @@ export default function Home() {
           <nav className="mb-4 flex items-center justify-between pl-1">
             <Breadcrumb />
             <SwitchLayout />
+            <Script src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></Script>
           </nav>
           <FileListing />
-          <Script src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></Script>
         </div>
       </main>
-      <Script src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></Script>
       <Footer />
-      <Script src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></Script>
     </div>
   )
 }
