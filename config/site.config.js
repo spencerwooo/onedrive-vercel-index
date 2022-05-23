9583b7afad8e2314589a454432b19bea3402c83f
bigbaidog <1194423126@qq.com>
2022-05-23 20:36:53 +0800

    修改网页标题

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  title: "BigBaiDog's OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
