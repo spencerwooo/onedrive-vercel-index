658dd0f2119956fb44aa821d8c3ecd466c8a1fd3
cbdgftqs <42031983+cbdgftqs@users.noreply.github.com>
2022-06-02 12:50:06 +0800

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -15,7 +15,7 @@ module.exports = {
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
-  redirectUri: 'http://localhost:53682/',
+  redirectUri: 'http://localhost',
 
   // These are the URLs of the OneDrive API endpoints. You would not need to change anything here if you are using OneDrive International
   // or E5 Subscription OneDrive for Business. You may need to change these if you are using OneDrive 世纪互联.
