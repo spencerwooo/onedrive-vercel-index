ef738f56b4a7c263673a2bd980eb480ec15f3110
Adlidziliqram <100081742+Adlidziliqram@users.noreply.github.com>
2022-07-07 23:44:56 +0700

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -19,8 +19,8 @@ module.exports = {
 
   // These are the URLs of the OneDrive API endpoints. You would not need to change anything here if you are using OneDrive International
   // or E5 Subscription OneDrive for Business. You may need to change these if you are using OneDrive 世纪互联.
-  authApi: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
-  driveApi: 'https://graph.microsoft.com/v1.0/me/drive',
+  authApi: "https://login.partner.microsoftonline.cn/common/oauth2/v2.0/token",
+  driveApi: "https://microsoftgraph.chinacloudapi.cn/v1.0/me/drive",
 
   // The scope we require are listed here, in most cases you would not need to change this as well.
   scope: 'user.read files.read.all offline_access',
