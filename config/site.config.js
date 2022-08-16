347196efb0ad13d6d6ac7c3aee0173bce72d8601
柠萌 <66536447+lemonfunny@users.noreply.github.com>
2022-08-16 16:44:43 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -53,7 +53,7 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      link: 'https://github.com/spencerwooo/onedrive-vercel-ind',
     },
     {
       name: 'Telegram',
