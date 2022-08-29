a306124be716c5772177cf6c6ecca4735205761a
xmcchv <38415380+xmcchv@users.noreply.github.com>
2022-08-29 13:42:22 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -45,7 +45,7 @@ module.exports = {
   protectedRoutes: ['/涩涩','/个人文件','/GoodNotes','/Notability'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: '',
+  email: "",
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
