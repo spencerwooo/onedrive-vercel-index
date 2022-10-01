29bdebc43781ce20874ebd4ae15cafc9b21e64cb
yuxinshan <xinshan7282@gmail.com>
2022-10-01 20:58:07 +0800

    change title

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  title: "Tiddler's Drive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public/',
