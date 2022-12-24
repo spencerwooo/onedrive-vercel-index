d8931b010d32a764ee11177f9f3678bb39dcc564
ChenShengWei <70498029+jack132860@users.noreply.github.com>
2022-12-25 09:31:26 +1000

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar = () => {
     setTokenPresent(storedToken())
   }, [])
 
-  {/*const { t } = useTranslation()*/}
+  //const { t } = useTranslation()
 
   const clearTokens = () => {
     setIsOpen(false)
@@ -89,7 +89,7 @@ const Navbar = () => {
             </div>
           </button>
 
-          {/*<SwitchLang />*/}
+          </*SwitchLang */>
 
           {siteConfig.links.length !== 0 &&
             siteConfig.links.map((l: { name: string; link: string }) => (
