fccce2df42c465694f8e6f94c0085137a1465de0
Rigolino1 <106283533+Rigolino1@users.noreply.github.com>
2023-01-10 23:37:00 -0600

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -20,7 +20,7 @@ module.exports = {
   title: "Rigo OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/',
+  baseDirectory: '/Public',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
