85d0d679bd2dfe7db3fd20b97cf48eae3ee80274
spencerwooo <spencer.woo@outlook.com>
2023-01-26 13:48:09 +0800

    fix: upgrade font awesome 6.0 and resolve ssr problem

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
 import '../styles/globals.css'
 import '../styles/markdown-github.css'
 
+// Require had to be used to prevent SSR failure in Next.js
+// Related discussion: https://github.com/FortAwesome/Font-Awesome/issues/19348
+const { library, config } = require('@fortawesome/fontawesome-svg-core')
 config.autoAddCss = false
 
-import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faFileImage,
   faFilePdf,
