cfae2b2cbf9d62cb71ba25e16a599877921f874d
bioogo <129346787+bioogo@users.noreply.github.com>
2023-04-03 15:55:40 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -45,7 +45,7 @@ module.exports = {
   protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:chenxiaoweivb@live.com',
+  email: 'mailto:408423394@qq.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
