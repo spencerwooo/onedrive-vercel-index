8a01d5905f36c8b9979e0fead19fd8909f2096c7
Vulcanraven <11424186+Vulcanraven91@users.noreply.github.com>
2023-06-02 13:47:20 +0200

    Added support for the 'de-DE' language variant (informal) (#1008)
    This commit adds support for the German language variant 'de-DE' in the application. The following changes were made:
    
    Created the '/public/locales/de-DE/common.json' file for the 'de-DE' language
    Modified the 'SwitchLang.tsx' component to include the 'de-DE' language option
    Updated the 'localeText' function to display ' Deutsch' for 'de-DE'
    These changes enable the application to provide localization in German for the informal 'de-DE' language variant. The newly created '/public/locales/de-DE/common.json' file contains the necessary translations for the German language. The 'SwitchLang.tsx' component has been updated to include the German language option ('de-DE') with the appropriate flag and language name.
    
    Users can now select the informal German language ('de-DE') to interact with the application.
    
    Co-authored-by: 🍌🐒 <nino@mohr.pw>

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'es', 'zh-CN', 'hi', 'id', 'tr-TR', 'zh-TW']
+    locales: ['de-DE', 'en', 'es', 'zh-CN', 'hi', 'id', 'tr-TR', 'zh-TW']
   },
   localePath: path.resolve('public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
