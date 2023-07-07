50cad7c831693eb18a6544c4b51bb417cd55d576
eks <11900611@qq.com>
2023-07-07 15:53:05 +0800

    Update step-3.tsx

--- src/pages/onedrive-vercel-index-oauth/step-3.tsx
+++ src/pages/onedrive-vercel-index-oauth/step-3.tsx
@@ -228,8 +228,8 @@ export async function getServerSideProps({ query, locale }) {
   const { authCode } = query
 
   // 检查是否已经通过OAuth认证
-  const accessToken = await getAccessToken();
-  if (accessToken) {
+  const existingAccessToken = await getAccessToken();
+  if (existingAccessToken) {
     // 如果已经通过OAuth认证，重定向到主页
     return {
       redirect: {
